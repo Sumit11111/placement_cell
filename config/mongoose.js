87be3aa083a97353e49b147d37e87ceb8597e702
sumit11111 <sumitdhanda1111@gmail.com>
2023-02-05 16:34:08 +0530

    db hosted

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 //imprting mongoose
 const mongoose=require('mongoose');
 //connectin setup
-mongoose.connect('mongodb://127.0.0.1:27017/placement_cell');
+mongoose.connect('mongodb+srv://sumit:DuROeBFhXvtrDJgU@cluster0.eoh5n.mongodb.net/test?authMechanism=DEFAULT');
 const db=mongoose.connection;
 //opening db connection
 db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
