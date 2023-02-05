60bf1b0ed284d016935e747bf9ee2af9a8997361
sumit11111 <sumitdhanda1111@gmail.com>
2023-02-05 17:02:15 +0530

    all done

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 //imprting mongoose
 const mongoose=require('mongoose');
 //connectin setup
-mongoose.connect('mongodb+srv://sumit:DuROeBFhXvtrDJgU@cluster0.eoh5n.mongodb.net/test?authMechanism=DEFAULT');
+mongoose.connect(`mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.eoh5n.mongodb.net/test?authMechanism=DEFAULT`);
 const db=mongoose.connection;
 //opening db connection
 db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
